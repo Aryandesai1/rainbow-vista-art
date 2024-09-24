5cea78b72bc0c998b2bf4ee331169b9a30db92b6
aryandesai1 <aryandesai34@gmail.com>
2024-09-24 14:18:52 -0400

    changing about us and description

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,8 +6,7 @@ const AboutUs = () => {
     <section id="about-us" className="section">
       <h2>About Us</h2>
       <p>
-        We are Rainbow Vista Art, dedicated to bringing vibrant theatrical performances to life.
-        Our mission is to captivate and inspire audiences through our colorful and dynamic shows.
+      We are Rainbow Vista Art, dedicated to bringing vibrant theatrical performances, music concerts, celebrity appearance, navaratri -holi-diwali special programmes, destination weddings etc. Our mission is to captivate and inspire audiences through our colorful and dynamic shows.
       </p>
       
     </section>
