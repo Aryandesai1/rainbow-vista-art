e6cdaf7420cb58cff664f5dba3f23fe052973394
Aryandesai1 <77978364+Aryandesai1@users.noreply.github.com>
2025-01-18 09:54:44 +0530

    Poster and Contact change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,8 +9,7 @@ const Contact = () => {
       <h2>Contact Us</h2>
       
       <a href="mailto:rainbowvistaart@gmail.com">Email: rainbowvistaart@gmail.com</a>
-      <h3>Sejal Patel: 416-856-9326</h3>
-      <h3>Charul Desai: 905-586-5818</h3>
+      <h3>+1(647)-335-5753</h3>
     </section>
   );
 }
